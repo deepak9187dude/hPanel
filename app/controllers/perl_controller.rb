d148b370786663749497e5d0a6a0b51a4b8aabf6
deepak <deepak@deepak.(none)>
2011-11-22 12:20:36 +0530

    perl change

--- app/controllers/perl_controller.rb
+++ app/controllers/perl_controller.rb
@@ -1,6 +1,6 @@
 class PerlController < ApplicationController
   def index
-    render :text=> `perl #{Rails.root}/perls/datetime.pl localhost root sahilb`
+    render :text=> `perl #{Rails.root}/perls/datetime.pl param1`
   end
 
 end
