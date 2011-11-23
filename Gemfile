52f4c7b240cea4bff23a8eb98d3ffc03ba0172c8
Pardeep Dhingra <pd@Pardeep.(none)>
2011-11-23 10:51:41 +0530

    Modified bluepay

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.0.9'
 gem 'mysql2'
 gem 'jquery-rails'
 gem "paypal_adaptive", "~> 0.2.5"
+gem 'bluepay' ,"~> 1.0.1"
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- app/controllers/blue_pay_basic_controller.rb
+++ app/controllers/blue_pay_basic_controller.rb
@@ -1,22 +1,24 @@
 class BluePayBasicController < ApplicationController
-        require 'Bluepay'
              
+        require 'Bluepay'
+        
         def DoBluePayPayment
             #invoice = params[:invoice_details]
-            
+             
             accountId = get_gateway_settings('gateway_login', 2) #ACCOUNT_ID
             secretKey = get_gateway_settings('gateway_key', 2) #SECRET_KEY
            
-            bpApi = Bluepay.new(accountId.to_s, secretKey.to_s)
+            bpApi = Bluepay.new('100055017580', 'X403IXYR3M2Q2DNEZQ56CWOOF4XMOJQK')
             bpApi.customer_data("Pardeep","Dhingra","Town","Hall","NY","ad4512")
-            bpApi.easy_sale("1.00")
-            bpApi.use_card("4111111111111111", "1109", "123")
+            bpApi.use_card("4111111111111111", "1013", "112")
+            bpApi.easy_sale("1")
+
             bpApi.process()
 
             puts "Status: " + bpApi.get_status()
             puts "Message: " + bpApi.get_message()
-  #         puts "Transaction ID: " + bpApi.get_trans_id()
-            render :text=> "Status: " + bpApi.get_status() + "</br> Message: " +bpApi.get_message()
+           puts "Transaction ID: " + bpApi.get_trans_id()
+            render :text=> "Status: " + bpApi.get_status() + "</br> Message: " +bpApi.get_message() + ":" + bpApi.get_trans_id()
         end
 
         def get_gateway_settings(field,gateway_id)
